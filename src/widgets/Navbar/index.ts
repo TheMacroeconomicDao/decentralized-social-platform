868255e03c8cb10414f2c59e6caa80d9652e1a86
GyberExperiment <themacroeconomicdao@tuta.io>
2025-07-08 05:10:17 +0300

    DSP: Переключил импорт на NavbarEnhanced - теперь используется наша версия

--- src/widgets/Navbar/index.ts
+++ src/widgets/Navbar/index.ts
@@ -1,12 +1,10 @@
-import { Navbar } from "./ui/Navbar/Navbar";
-import { NavbarEnhanced } from "./ui/Navbar/Navbar-Enhanced";
+import { NavbarEnhanced as Navbar } from "./ui/Navbar/Navbar-Enhanced";
 import {NavbarTablet} from "./ui/NavbarTablet/NavbarTablet"
 import { NavbarMobile } from "./ui/NavbarMobile/NavbarMobile";
 import navbarItems from "./data/routesData";
 
 export {
     Navbar,
-    NavbarEnhanced,
     NavbarTablet,
     NavbarMobile,
     navbarItems
